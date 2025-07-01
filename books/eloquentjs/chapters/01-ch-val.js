a0dcb0009f0b3f26ac432da0c2c049cffd70a011
Ivan Ma <iv.ma@tcsbank.ru>
2025-07-01 12:54:11 +0300

    chapter: add chapter 1 - val, types & operators

---
+++ books/eloquentjs/chapters/01-ch-val.js
@@ -0,0 +1,16 @@
+let backtick = `new
+day
+in my life
+
+`;
+console.log(backtick);
+
+let lineEsc = "here\" goes \nnew line \n";
+console.log(lineEsc);
+
+let tabEsc = 'here goes \ttabulation';
+console.log(tabEsc);
+
+console.log("A newline character is written like \"\\n\".");
+
+console.log('"hey!"');
