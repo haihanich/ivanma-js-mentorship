76de0a859fd80b11da6907eb3065ea4602ee0d10
Ivan Ma <haihanich@gmail.com>
2025-07-14 20:25:59 +0300

    chapter: add experiments

--- books/eloquentjs/experiments/01-ch-val.js
+++ books/eloquentjs/experiments/01-ch-val.js
@@ -19,4 +19,39 @@ let first = "1";
 let second = "one";
 let sum = first + second + `please count ${50/2}`;
 
-console.log(typeof sum, sum);
+console.log(typeof sum, sum, '\n');
+
+console.log(typeof 13);
+console.log(typeof "hello");
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof null);
+
+console.log(100 + 2 * 4);
+console.log((100 + 2) * 4);
+console.log(100 % 12);
+console.log(2e2);
+console.log(2e4);
+
+console.log(0.1 + 0.2 !== 0.3);
+console.log(1 + 2 === 3);
+
+console.log("5" == 5);
+console.log("5" === 5);
+
+console.log("Z" < "a");
+console.log(NaN == NaN, '\n');
+
+console.log(1 || 2);
+console.log(1 && 2);
+console.log(true || false);
+console.log(true && false);
+console.log(true || true);
+console.log(true && true);
+console.log(false || false);
+console.log(false && false);
+
+
+
+
+
